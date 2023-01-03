50f629bdf24a2ffa199ccb4044b4fa57ecf0a94e
OnurK.Dev <onurk.dev@outlook.com>
2023-01-03 23:33:20 +0100

    Websockets IO Socket emit

--- src/index.js
+++ src/index.js
@@ -27,15 +27,20 @@ const publicDirectoryPath = path.join(__dirname, '../public');
 app.use(express.static(publicDirectoryPath))
 app.use(cors())
 app.use(express.json())
-io.on('connection', ()=>{
+
+let count = 0
+
+io.on('connection', (socket)=>{
     console.log('New WebSocket connection')
-    // socket.emit('message', 'Welcome!')
-    // socket.broadcast.emit('message', 'A new user has joined!')
-    // socket.on('sendMessage', (message)=>{
-    //     io.emit('message', message)
-    // })
-    // socket.on('disconnect', ()=>{
-    //     io.emit('message', 'A user has left!')
+
+    socket.emit('message', 'Welcome!')
+    socket.on('sendMessage', (message)=>{
+        io.emit('message', message)
+    })
+    // socket.emit('countUpdated', count)
+    // socket.on('increment', ()=>{
+    //     count++
+    //     io.emit('countUpdated', count)
     // })
 }
 )
